ad686fa51d30d3bc461638ca8937cba9f05a8d3a
Esteban Vera <luis.vera@avaldigitallabs.com>
2019-04-26 11:03:54 -0500

    add rest

--- helpers.ts
+++ helpers.ts
@@ -1,6 +1,19 @@
+export const DEFAULT_VALUE = 0;
+
 export function multiply(param1, param2): number {
-    return param1 * param2;
+
+    if ((typeof param1 !== 'number') || typeof param2 !== 'number'){
+        return DEFAULT_VALUE;
+    }
+
+    return isNullOrUndefined(param1) || isNullOrUndefined(param2) ? DEFAULT_VALUE : param1 * param2;
 }
+
+
+function isNullOrUndefined(el: any): boolean {
+    return el === null || el === undefined;
+}
+
 
 
 
--- spec/multiply.spec.ts
+++ spec/multiply.spec.ts
@@ -1,11 +1,55 @@
 import 'jasmine';
-import { multiply } from '../helpers';
+import { DEFAULT_VALUE, multiply } from '../helpers';
 
 describe('Prueba de multiplicar', () => {
 
     it('Debe regresar la multiplicación', () => {
 
        expect(multiply(2, 3)).toBe(6);
+
+       expect(multiply(2, 5)).toBe(10);
+
+       expect(multiply(3, 0)).toBe(0);
+
+    });
+
+    it('Debe regresar 0 si los parametros son nulos o indefinidos', () => {
+
+        expect(multiply(null, 3)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(3, null)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(undefined, 1)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(2, undefined)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(null, undefined)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(undefined, null)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(undefined, undefined)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(null, null)).toBe(DEFAULT_VALUE);
+
+    });
+
+    it('Debe regresar 0 si los parametros son diferenres a tipo number', () => {
+
+        expect(multiply('hola', 3)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(45, 'hey')).toBe(DEFAULT_VALUE);
+
+        expect(multiply('tomalo', 'hey')).toBe(DEFAULT_VALUE);
+
+        expect(multiply(true, 'hey')).toBe(DEFAULT_VALUE);
+
+        expect(multiply('hey', true)).toBe(DEFAULT_VALUE);
+
+        expect(multiply([0, 1 ,2 ], false)).toBe(DEFAULT_VALUE);
+
+        expect(multiply(true, { name: 'tomalo' })).toBe(DEFAULT_VALUE);
+
+        expect(multiply(2, false)).toBe(DEFAULT_VALUE);
 
     });
 
